0e34400d215b2a250523190b2d006037681ac01e
Sagar Yadav <sagardxd5@gmail.com>
2024-04-29 14:03:53 +0530

    vercel

--- apps/user-app/app/(dashboard)/transfer/page.tsx
+++ apps/user-app/app/(dashboard)/transfer/page.tsx
@@ -38,7 +38,7 @@ export default async function() {
     const transactions = await getOnRampTransactions();
 
     return <div className="w-screen">
-        <div className="text-4xl text-[#6a51a6] pt-8 mb-8 font-bold">
+        <div className="text-4xl text-[#484ec5] pt-8 mb-8 font-bold">
             Transfer
         </div>
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 p-4">
